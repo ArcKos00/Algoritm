6cfe417168f9dd70b9eeefec655d0bbaa67c76fe
ArcKos00 <105163313+ArcKos00@users.noreply.github.com>
2023-05-06 18:45:10 +0300

    Added

--- Readme.md
+++ Readme.md
@@ -1,1 +1,5 @@
-Here is my collection of sorting algorithms
+The Shell Sort Algoritm O(n*log n)
+
+The Shell sort algorithm (English Shell Sort) is a modification of the insertion sort algorithm, in which reducing the step between elements allows more efficient insertion of elements into the sorted part of the array.
+
+The essence of the algorithm is that the array is divided into subarrays using a "step", which decreases with each sorting step. An insertion sort algorithm is applied to each subarray. They usually start with a large step and reduce it to 1.
--- Sort.cs
+++ Sort.cs
@@ -4,5 +4,28 @@ namespace Algoritms;
 
 public class Algoritm
 {
-    
+    static void ShellSort<T>(T[] arr)
+    {
+        int n = arr.Length;
+        int gap = n / 2;
+
+        while (gap > 0)
+        {
+            for (int i = gap; i < n; i++)
+            {
+                var temp = arr[i];
+                int j = i;
+
+                while (j >= gap && arr[j - gap].CompareTo(temp) > 0)
+                {
+                    arr[j] = arr[j - gap];
+                    j -= gap;
+                }
+
+                arr[j] = temp;
+            }
+
+            gap /= 2;
+        }
+    }
 }

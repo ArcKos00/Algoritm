c1bc3cfea575d1032e117d7956708946d02d0a84
ArcKos00 <105163313+ArcKos00@users.noreply.github.com>
2023-05-06 19:25:39 +0300

    Added

--- Readme.md
+++ Readme.md
@@ -1,1 +1,5 @@
-Here is my collection of sorting algorithms
+The Coctail Shake Sort Algorithm O(n^2)
+
+The cocktail shaker sort algorithm with reflection (Cocktail Shaker Sort) is an improved variant of the usual bubble sort algorithm, which works faster in certain cases. This algorithm is also called shuffle sort algorithm.
+
+The basic idea is that the algorithm moves through the array from left to right, just like a normal bubble sort algorithm, and then goes back from right to left, checking the elements from the end of the array. This process continues until all elements of the array are sorted.
--- Sort.cs
+++ Sort.cs
@@ -4,5 +4,46 @@ namespace Algoritms;
 
 public class Algoritm
 {
+    public static void CoctailShakeSort<T>(T[] arr)
+    {
+        var swapped = true;
+        int start = 0;
+        int end = arr.Length - 1;
     
+        while (swapped)
+        {
+            swapped = false;
+    
+            for (int i = start; i < end; ++i)
+            {
+                if (arr[i].CompareTo(arr[i + 1]) > 0)
+                {
+                    var temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+    
+            if (!swapped)
+            {
+                break;
+            }
+    
+            swapped = false;
+    
+            for (int i = end - 1; i >= start; --i)
+            {
+                if (arr[i].CompareTo(arr[i + 1]) > 0)
+                {
+                    var temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+    
+            end--;
+        }
+    }
 }

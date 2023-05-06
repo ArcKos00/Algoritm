564d681068bd4f80344db58d8dd039855735e50e
ArcKos00 <105163313+ArcKos00@users.noreply.github.com>
2023-05-06 13:13:46 +0300

    Added

--- Readme.md
+++ Readme.md
@@ -1,1 +1,3 @@
-Here is my collection of sorting algorithms
+The Merge Sort Algoritm O(n*log n)
+
+This algorithm sorts the array "arr" using recursive merge sort. The algorithm first splits the array in half and then sorts each half of the array by recursively calling the MergeSort() function. It then merges the sorted parts of the array using the Merge() function.
--- Sort.cs
+++ Sort.cs
@@ -4,5 +4,48 @@ namespace Algoritms;
 
 public class Algoritm
 {
-    
+    public static void MergeSort<T>(T[] arr, int left, int right)
+        where T : IComparable<T>
+    {
+        if (left < right)
+        {
+            int middle = (left + right) / 2;
+            MergeSort(arr, left, middle);
+            MergeSort(arr, middle + 1, right);
+
+            var temp = new T[arr.Length];
+
+            for (int i = left; i<= right; i++)
+            {
+                temp[i] = arr[i];
+            }
+
+            int i = left;
+            int j = middle + 1;
+            int k = left;
+            
+            while (i <= middle && j <= right)
+            {
+                if (temp[i].CompareTo(temp[j]) <= 0)
+                {
+                    arr[k] = temp[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = temp[j];
+                    j++;
+                }
+
+                k++;
+            }
+
+            while (i <= middle)
+            {
+                arr[k] = temp[i];
+                k++;
+                i++;
+            }
+        }
+    }
 }

d03d15323e2effb954007d8392513af57dd04712
ArcKos00 <105163313+ArcKos00@users.noreply.github.com>
2023-05-06 19:04:04 +0300

    Added

--- Readme.md
+++ Readme.md
@@ -1,1 +1,5 @@
-Here is my collection of sorting algorithms
+The Heap Sort Algoritms O(n*log n)
+
+Heap Sort is an efficient sorting algorithm that uses the heap data structure. Heap sorting works in O(n * log n) time, where n is the size of the array.
+
+The essence of the algorithm is that the array is turned into a heap (a binary tree in which each node is greater than or equal to its child nodes) and the maximum element is removed from it. After that, the maximum element is inserted into the sorted part of the array, and the rest of the array is turned into a heap again and the process is repeated until all elements are sorted.
--- Sort.cs
+++ Sort.cs
@@ -4,5 +4,48 @@ namespace Algoritms;
 
 public class Algoritm
 {
-    
+    public static void HeapSort<T>(T[] arr)
+    {
+        int n = arr.Length;
+        for (int i = n / 2 - 1; i >= 0; i--)
+        {
+            Heapify(arr, n, i);
+        }
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            var temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            Heapify(arr, i, 0);
+        }
+    }
+
+    public void Heapify<T>(T[] arr, int n, int i)
+        where T : IComparable<T>
+    {
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+
+        if (l < n && arr[l].CompareTo(arr[largest]) > 0)
+        {
+            largest = l;
+        }
+
+        if (r < n && arr[r].CompareTo(arr[largest]) > 0)
+        {
+            largest = r;
+        }
+
+        if (largest != i)
+        {
+            var temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+
+            Heapify(arr, n, largest);
+        }
+    }
 }
